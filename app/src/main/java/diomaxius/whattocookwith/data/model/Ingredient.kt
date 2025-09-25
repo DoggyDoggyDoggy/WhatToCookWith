4007a63f1f313129034b4bef0a3bdd5f7dc0f552
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-09-25 23:35:21 +1200

    IngredientCard draft

---
+++ app/src/main/java/diomaxius/whattocookwith/data/model/Ingredient.kt
@@ -0,0 +1,6 @@
+package diomaxius.whattocookwith.data.model
+
+data class Ingredient(
+    val name: String,
+    val quantity: Int
+)
---
+++ app/src/main/java/diomaxius/whattocookwith/ui/components/IngredientCard.kt
@@ -0,0 +1,79 @@
+package diomaxius.whattocookwith.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import diomaxius.whattocookwith.data.model.Ingredient
+
+@Composable
+fun IngredientCard(
+    ingredient: Ingredient
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(6.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(
+                modifier = Modifier.width(6.dp)
+            )
+
+            Text(
+                text = ingredient.name,
+                fontSize = 20.sp
+            )
+
+            Spacer(
+                modifier = Modifier.weight(1f)
+            )
+
+            Icon(
+                modifier = Modifier.size(32.dp),
+                imageVector = Icons.Default.Remove,
+                contentDescription = null
+            )
+
+            Spacer(
+                modifier = Modifier.width(6.dp)
+            )
+
+            Text(
+                text = ingredient.quantity.toString()
+            )
+
+            Spacer(
+                modifier = Modifier.width(6.dp)
+            )
+
+            Icon(
+                modifier = Modifier.size(32.dp),
+                imageVector = Icons.Default.Add,
+                contentDescription = null
+            )
+
+            Spacer(
+                modifier = Modifier.width(6.dp)
+            )
+        }
+    }
+}
