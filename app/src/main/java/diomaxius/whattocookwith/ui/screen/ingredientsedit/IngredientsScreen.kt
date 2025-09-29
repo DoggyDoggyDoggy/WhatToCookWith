022c34cf54da0e65b07101c0e312a0f7b3c7459e
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-09-29 20:12:15 +1300

    Refactor IngredientsScreen and add dialog state management
    This commit refactors the `IngredientsScreen` composable and introduces state management for the `AddIngredientDialog`.

--- app/src/main/java/diomaxius/whattocookwith/ui/screen/ingredientsedit/IngredientsScreen.kt
+++ app/src/main/java/diomaxius/whattocookwith/ui/screen/ingredientsedit/IngredientsScreen.kt
@@ -1,14 +1,11 @@
 package diomaxius.whattocookwith.ui.screen.ingredientsedit
 
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -17,7 +14,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import diomaxius.whattocookwith.domain.model.Ingredient
 import diomaxius.whattocookwith.ui.components.IngredientCard
 import diomaxius.whattocookwith.ui.screen.ingredientsedit.components.AddIngredientDialog
