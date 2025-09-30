d3e63183889859159c02e9a036f8174ea2e79051
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-09-30 15:20:52 +1300

    Add "+" icon to FAB
    This commit updates the FloatingActionButton in `IngredientsScreen.kt` to display a "+" icon using `Icons.Default.Add`.

--- app/src/main/java/diomaxius/whattocookwith/ui/screen/ingredientsedit/IngredientsScreen.kt
+++ app/src/main/java/diomaxius/whattocookwith/ui/screen/ingredientsedit/IngredientsScreen.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -32,7 +35,12 @@ fun IngredientsScreen(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = { openDialog = true }
-            ) {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+            }
         }
     ) { innerPadding ->
         Content(
