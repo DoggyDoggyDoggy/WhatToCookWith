28c11b8865331de34218cc491fd700ebfe11603d
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-02 02:26:29 +1300

    Refactor: Rename IncreaseQuantityIngredientUseCase
    This commit renames the `IncreaseQuantityIngredientUseCase` to `IncreaseIngredientQuantityUseCase` for better clarity and consistency in naming conventions.

--- app/src/main/java/diomaxius/whattocookwith/domain/usecase/IncreaseIngredientQuantityUseCase.kt
+++ app/src/main/java/diomaxius/whattocookwith/domain/usecase/IncreaseIngredientQuantityUseCase.kt
@@ -4,7 +4,7 @@ import diomaxius.whattocookwith.domain.model.Ingredient
 import diomaxius.whattocookwith.domain.repository.IngredientRepository
 import javax.inject.Inject
 
-class IncreaseQuantityIngredientUseCase @Inject constructor(
+class IncreaseIngredientQuantityUseCase @Inject constructor(
     private val ingredientRepository: IngredientRepository
 ) {
     suspend operator fun invoke(ingredient: Ingredient) =
