a34cdde5d8ce3f691ee77522b961e8aef303dacb
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-11 16:09:42 +1300

    feat: Add `RecipeWithIngredients` data class
    This commit introduces the `RecipeWithIngredients` data class to model the relationship between recipes and their ingredients.
    
    This class uses Room's `@Embedded` and `@Relation` annotations to define a one-to-many relationship, linking a `RecipeEntity` to its corresponding list of `RecipeIngredientEntity` objects.

---
+++ app/src/main/java/diomaxius/whattocookwith/data/model/RecipeWithIngredients.kt
@@ -0,0 +1,11 @@
+package diomaxius.whattocookwith.data.model
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class RecipeWithIngredients(
+    @Embedded val recipe: RecipeEntity,
+    @Relation(parentColumn = "id", entityColumn = "recipeId")
+    val ingredients: List<RecipeIngredientEntity>
+)
+
