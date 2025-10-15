275b67f6896ee443333e23e9853446a5a90df47d
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-15 22:55:34 +1300

    feat: Display recipe ingredients in AddRecipeScreen
    This commit introduces a new section to the `AddRecipeScreen` for displaying and managing recipe ingredients.
    
    -   Adds a `Column` to hold the ingredients list.
    -   Includes a `Row` with a "Recipe ingredients:" title and an "Add" `IconButton` for adding new ingredients.
    -   Utilizes a `LazyColumn` to display a list of `IngredientCard` composables, representing the ingredients currently in the recipe.

--- app/src/main/java/diomaxius/whattocookwith/ui/screen/addrecipe/AddRecipeScreen.kt
+++ app/src/main/java/diomaxius/whattocookwith/ui/screen/addrecipe/AddRecipeScreen.kt
@@ -2,18 +2,30 @@ package diomaxius.whattocookwith.ui.screen.addrecipe
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,6 +35,7 @@ import diomaxius.whattocookwith.domain.model.RecipeIngredient
 import diomaxius.whattocookwith.navigation.LocalNavController
 import diomaxius.whattocookwith.ui.components.PopBackArrowButton
 import diomaxius.whattocookwith.ui.components.TopBar
+import diomaxius.whattocookwith.ui.components.ingredientcard.IngredientCard
 
 const val RECIPE_NAME_PLACEHOLDER = "For example: Pancakes"
 const val RECIPE_Instructions_PLACEHOLDER = """For example:
@@ -116,6 +129,48 @@ fun Content(
                 onValueChange = { onRecipeInstructionsChange(it) },
                 placeholder = { Text(RECIPE_Instructions_PLACEHOLDER) }
             )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Recipe ingredients:",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Medium
+                )
+                IconButton(
+                    modifier = Modifier.size(48.dp),
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.tertiaryContainer
+                    ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add ingredient",
+                        tint = MaterialTheme.colorScheme.onTertiaryContainer,
+                    )
+                }
+            }
+            LazyColumn {
+                items(recipeIngredients, key = { it.ingredientName }) {
+                    IngredientCard(
+                        ingredient = Ingredient(
+                            name = it.ingredientName,
+                            quantity = it.requiredQuantity.toInt(), // Maybe refactor Ingredient model quantity to Int data type
+                            unit = it.unit
+                        ),
+                        actions = {}
+                    )
+                }
+            }
         }
     }
 }
