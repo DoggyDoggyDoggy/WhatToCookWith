7dfa4156ec78961c9fe8431cd660a04527f684ee
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-21 13:09:43 +1300

    Refactor: Enhance RecipeScreen UI
    This commit refactors the `RecipeScreen` to improve its structure and visual presentation.
    
    -   Integrates a `Scaffold` with a `TopBar` that displays the recipe's name and includes a back navigation button.
    -   Creates a dedicated `Content` composable to hold the recipe details.
    -   Wraps the recipe instructions `Text` within a `Card` for better visual grouping.
    -   Applies padding and sets the card's `containerColor` to `MaterialTheme.colorScheme.primaryContainer`.

--- app/src/main/java/diomaxius/whattocookwith/ui/screen/recipe/RecipeScreen.kt
+++ app/src/main/java/diomaxius/whattocookwith/ui/screen/recipe/RecipeScreen.kt
@@ -1,18 +1,66 @@
 package diomaxius.whattocookwith.ui.screen.recipe
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import diomaxius.whattocookwith.domain.model.Recipe
+import diomaxius.whattocookwith.navigation.LocalNavController
+import diomaxius.whattocookwith.ui.components.PopBackArrowButton
+import diomaxius.whattocookwith.ui.components.TopBar
 
 @Composable
 fun RecipeScreen(
-    viewModel: RecipeScreenViewModel = hiltViewModel()
+    viewModel: RecipeScreenViewModel = hiltViewModel(),
 ) {
     val recipe by viewModel.recipe.collectAsState()
 
-    Text(recipe.name)
+    val navHostController = LocalNavController.current
 
+    Scaffold(
+        topBar = {
+            TopBar(
+                text = recipe.name,
+                navigationButton = { PopBackArrowButton(navHostController) }
+            )
+        }
+    ) { innerPadding ->
+        Content(
+            modifier = Modifier.padding(innerPadding),
+            recipe = recipe
+        )
+    }
+}
+
+@Composable
+fun Content(
+    modifier: Modifier = Modifier,
+    recipe: Recipe,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        ) {
+            Text(
+                modifier = Modifier.padding(12.dp),
+                text = recipe.instructions
+            )
+        }
+    }
 }
